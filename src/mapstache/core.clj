0bc79a1094c1bd40e1f280a51b416acfafc4dfe8
Darrell Hamilton <darrell.noice@gmail.com>
2014-05-08 21:49:36 -0700

    Base implementation

--- src/mapstache/core.clj
+++ src/mapstache/core.clj
@@ -1,6 +1,75 @@
-(ns mapstache.core)
+(ns mapstache.core
+  (:import clojure.lang.IFn
+           clojure.lang.ILookup
+           clojure.lang.IPersistentMap
+           clojure.lang.Seqable
+           clojure.lang.MapEntry
+           clojure.lang.IPersistentCollection
+           clojure.lang.IPersistentVector
+))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(declare mapstache)
+
+(defprotocol IRender (render [_ str data]))
+
+(deftype Mapstache [^mapstache.core.IRender renderer
+                    ^IPersistentMap value
+                    ^IPersistentVector cursor
+                    ^IPersistentVector lookups
+                    root]
+  ILookup
+  (valAt [this k] (.valAt this k nil))
+  (valAt [this k not-found]
+    (let [lookup-key (conj cursor k)
+          v (get-in value (conj cursor k) not-found)]
+      (cond
+       (instance? IPersistentMap v) (mapstache renderer value lookup-key lookups root)
+       (instance? String v) (render renderer v (or root this))
+       :else v)))
+
+  IFn
+  (invoke [this k] (.valAt this k))
+  (invoke [this k not-found] (.valAt this k not-found))
+  (toString [this] (str [value cursor lookups root]))
+  Seqable
+  (seq [this]
+    (map (fn [k] (MapEntry. k (.valAt this k))) (keys (get-in value cursor))))
+
+  IPersistentCollection
+  (count [this] (count (get-in value cursor)))
+  (empty [this] (empty (get-in value cursor)))
+  (equiv [this o]
+    (let [my-value (get-in value cursor)]
+      (if (instance? Mapstache o)
+        (= my-value
+           (get-in (.value o) (.cursor o)))
+        (= my-value o))))
+  (cons [this o]
+    (let [new-value (if (empty cursor)
+                      (conj value o)
+                      (update-in value
+                          cursor
+                          (fn [v] (conj v o))))]
+
+      (mapstache renderer new-value cursor lookups root)))
+
+  IPersistentMap
+  (assoc [this k v]
+    (mapstache renderer (assoc-in value (conj cursor k) v) cursor lookups root))
+  (assocEx [this k v]
+    (if (contains? k (get-in value cursor))
+      (throw (IllegalArgumentException. (str "Already contains key: " k)))
+      (assoc this k v)))
+
+  (without [this k]
+    (let [new-value (if (empty cursor)
+                      (dissoc value k)
+                      (update-in value cursor #(dissoc k)))]
+      (mapstache renderer new-value cursor lookups root))))
+
+
+(defn mapstache
+  ([renderer value]
+     (mapstache renderer value [] (atom []) nil))
+  ([renderer value cursor lookups root]
+     (Mapstache. renderer value cursor lookups root)))
--- test/mapstache/core_test.clj
+++ test/mapstache/core_test.clj
@@ -1,7 +1,56 @@
 (ns mapstache.core-test
   (:require [clojure.test :refer :all]
-            [mapstache.core :refer :all]))
+            [mapstache.core :refer :all])
+  (:import mapstache.core.Mapstache
+           clojure.lang.MapEntry))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(defn matching-maps [m]
+  [m (mapstache (reify IRender (render [_ s d] s)) m)])
+
+(deftest behaves-like-a-map
+  (testing "I can query it with a keyword."
+    (let [[m ms] (matching-maps {:a "value"})]
+      (is (= (:a ms) (:a m)))
+      (is (= (:not-a ms) (:not-a m)))))
+
+  (testing "I can call it like a function"
+    (let [[m ms] (matching-maps {:a "value"})]
+      (is (= (ms :a) (m :a)))
+      (is (= (ms :not-a) (m :not-a)))))
+
+  (testing "I can call map over it"
+    (let [[m ms] (matching-maps {:a "value" :b "other-value"})]
+      (is (= (map identity ms) (map identity m)))))
+
+  (testing "I can compare a Mapstache to things"
+    (let [[m ms] (matching-maps {:a "value"})]
+      (is (= ms ms))
+      (is (= ms m))
+      (is (not (= m ms)) "I can't make map know how to check a Mapstache")
+      (is (not (= ms "not a map")))))
+
+  (testing "I can conj elements onto it"
+    (let [[m ms] (matching-maps {:a "value"})
+          v [:b "other-value"]]
+      (is (= (conj ms v) (conj m v)))))
+
+  (testing "I can assoc elements into it"
+    (let [[m ms] (matching-maps {:a "value"})
+          k :key
+          v :value]
+      (is (= (assoc ms k v) (assoc m k v)))))
+
+  (testing "I can dissoc elements into it"
+    (let [[m ms] (matching-maps {:a "value" :b "other-value"})
+          k :b]
+      (is (= (dissoc ms k) (dissoc m k))))))
+
+(deftest mapstache-behavior
+  (testing "sub-maps are returned as Mapstache isntances"
+    (let [[m ms] (matching-maps {:a {:b "value"}})]
+      (is (instance? Mapstache (:a ms)))
+      (is (= (:a ms) (:a m)))))
+  (testing "Strings get templated"
+    (let [ms (mapstache (reify IRender (render [_ s d] 42)) {:a "{{ str }}"})]
+      (is (= (:a ms) 42))))
+)
