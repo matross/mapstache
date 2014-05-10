29b02f61a620a5f3d8c16bff5ffc9ac6850c75ea
Darrell Hamilton <darrell.noice@gmail.com>
2014-05-10 00:13:16 -0700

    tweaks and filling out project meta data

--- project.clj
+++ project.clj
@@ -1,8 +1,13 @@
 (defproject matross/mapstache "0.1.0-SNAPSHOT"
-  :description "A specialized map impl that renders string values as templates."
+  :description "More expressive end user configs via templated map values"
   :url "https://github.com/matross/mapstache"
   :license {:name "MIT"
             :url "http://opensource.org/licenses/MIT"}
+  :scm {:name "git"
+        :url "https://github.com/matross/mapstache"}
+  :pom-addition [:developers [:developer {:id "zeroem"}
+                              [:name "Darrell Hamilton"]
+                              [:url "https://github.com/zeroem"]]]
   :dependencies [[org.clojure/clojure "1.5.1"]]
   :profiles {:dev {:dependencies [[de.ubercode.clostache/clostache "1.4.0"]]}}
   :plugins [[codox "0.8.0"]])
