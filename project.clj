d2d32a6e9ce2d532d883c548d6ca718f3237086e
Darrell Hamilton <darrell.noice@gmail.com>
2014-05-18 20:27:43 -0600

    fixing up the pom for deployment

--- project.clj
+++ project.clj
@@ -5,9 +5,14 @@
             :url "http://opensource.org/licenses/MIT"}
   :scm {:name "git"
         :url "https://github.com/matross/mapstache"}
-  :pom-addition [:developers [:developer {:id "zeroem"}
+  :pom-addition [:developers [:developer
+                              [:id "zeroem"]
                               [:name "Darrell Hamilton"]
-                              [:url "https://github.com/zeroem"]]]
+                              [:url "https://github.com/zeroem"]]
+                 [:developer
+                  [:id "eggsby"]
+                  [:name "Thomas Omans"]
+                  [:url "https://github.com/eggsby"]]]
   :dependencies [[org.clojure/clojure "1.5.1"]]
   :profiles {:dev {:dependencies [[stencil "0.3.3"]
 
