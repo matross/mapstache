eaf3387dc7959da71ea627ebf5c4324712de0cd3
Thomas Omans <thomas.omans@gmail.com>
2014-10-15 22:57:09 -0700

    bumping release version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject matross/mapstache "0.3.0-SNAPSHOT"
+(defproject matross/mapstache "0.3.0"
   :description "More expressive end user configs via templated map values"
   :url "https://github.com/matross/mapstache"
   :license {:name "MIT"
